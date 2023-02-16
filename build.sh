7f04344b6ef606be2f0f229179811f0151ad5812
hectorvax <hecvalve@gmail.com>
2023-02-16 14:23:50 +0100

    Fix compile error

--- build.sh
+++ build.sh
@@ -9,14 +9,14 @@ export SUBARCH=arm64
 export DTC_EXT=${HOME}/kernel/sweet_toolchain/dtc/dtc-1.4.4/prebuilt/dtc
 export KBUILD_BUILD_USER="hectorvax"
 export KBUILD_BUILD_HOST="perfmod"
-make O=${HOME}/sweet-r-kernel/out ARCH=arm64 sweet_user_defconfig
+make O=out ARCH=arm64 sweet_user_defconfig
 
-make -j$(nproc --all)  O=${HOME}/sweet-r-kernel/out \
+make -j$(nproc --all)  O=out
 PATH="${HOME}/kernel/sweet_toolchain/clang/LLVM-10.0.9/bin:${HOME}/kernel/sweet_toolchain/gcc/aarch64-linux-android-4.9//bin:${PATH}" \
                                 ARCH=arm64 \
                                 REAL_CC=clang \
                                 CROSS_COMPILE=aarch64-linux-android- \
-                                CLANG_TRIPLE=aarch64-linux-gnu- | tee ${HOME}/sweet-r-kernel/out/kernel.log
+                                CLANG_TRIPLE=aarch64-linux-gnu- 2>&1 | tee kernel.log
 End=$(date +"%s")
 Diff=$(($End - $Start))
 echo -e "$gre << Build completed in $(($Diff / 60)) minutes and $(($Diff % 60)) seconds >> \n $white"
